4b99e68ebafbeb3e99dcf59d8076974a14f78d75
yuin0 <yuinoforSK@gmail.com>
2021-06-30 00:13:05 +0900

    game_manager/block_controller_sample.py

--- game_manager/block_controller_sample.py
+++ game_manager/block_controller_sample.py
@@ -211,7 +211,7 @@ class Block_Controller(object): # object is not necessary (to use python2): Bloc
         #### maxDy
         #maxDy = max(BlockMaxY) - min(BlockMaxY)
         #### maxHeight
-        #maxHeight = max(BlockMaxY) - fullLines
+#       maxHeight = max(BlockMaxY) - fullLines
 
         ## statistical data
         #### stdY
@@ -228,12 +228,13 @@ class Block_Controller(object): # object is not necessary (to use python2): Bloc
 
         # calc Evaluation Value
         score = 0
-        #score = score + fullLines *  10.0           # try to delete line 
+#        if fullLines > 1:
+#            score = score + fullLines * 10.0         # try to delete line 
         score = score - nHoles * 10.0               # try not to make hole
         score = score - nIsolatedBlocks * 1.0      # try not to make isolated block
-        score = score - absDy * 1.0                # try to put block smoothly
+        score = score - absDy * 1.0                 # try to put block smoothly
         #score = score - maxDy * 0.3                # maxDy
-        #score = score - maxHeight * 5              # maxHeight
+#        score = score - maxHeight * maxHeight * 0.1              # maxHeight
         #score = score - stdY * 1.0                 # statistical data
         #score = score - stdDY * 0.01               # statistical data
 
